54f7974bf144df7063cb33cecbb0bc74f86ead29
Kate Pond <kate@thepondsedge.com>
2018-07-16 12:25:32 -0700

    adds a hardcoded countdown to post response

--- app/models/queued_color.rb
+++ app/models/queued_color.rb
@@ -2,4 +2,8 @@ class QueuedColor < ApplicationRecord
   validates :color, presence: true
   # validates :color, length: { in: 7..10 }
   # TODO: Readd length validation when input length is known
+
+  def countdown_time
+    return 10000
+  end
 end
