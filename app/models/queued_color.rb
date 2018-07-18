c2b4783a0d6b7b731d366ec9f77842495c5b6d4e
Kate Pond <kate@thepondsedge.com>
2018-07-17 21:29:55 -0700

    gets the countdown clock working locally... hallelujah

--- app/models/queued_color.rb
+++ app/models/queued_color.rb
@@ -1,8 +1,12 @@
 class QueuedColor < ApplicationRecord
+  require 'date'
   require 'rufus-scheduler'
   validates :color, presence: true
   # validates :color, length: { in: 7..10 }
   # TODO: Readd length validation when input length is known
+
+  # default interval time 30 seconds = 30000 (07/16/18)
+  DURATION_TIME = 10
 
   def self.color_change
     count = QueuedColor.count
@@ -11,7 +15,7 @@ class QueuedColor < ApplicationRecord
 
     if count > 0
 
-      scheduler.every '30s' do
+      @@job = scheduler.schedule_every("#{DURATION_TIME.to_s}s") do
         displayed_color = DisplayedColor.new(color: QueuedColor.first.color)
         displayed_color.save
 
@@ -24,13 +28,24 @@ class QueuedColor < ApplicationRecord
     end
   end
 
-  def countdown_time # default interval time 30 seconds = 30000 (07/16/18)
+  def countdown_time
     count = QueuedColor.count
-    if count > 2
-      time = (count - 2) * 30000
-      return time
-    elsif count == 2
-      return 30000 # placeholder until I do calculated time for remaining time
+    duration_seconds = (DURATION_TIME * 1000)
+
+    # calulates remaining time if there is any
+    if @@job.last_time == nil
+      remaining_time = duration_seconds - ((Time.now.to_i - QueuedColor.first.created_at.to_i) * 1000)
+    else
+      remaining_time = duration_seconds - ((Time.now.to_i - @@job.last_time.seconds) * 1000)
+    end
+
+    # calculates the countdown time
+    if count == 2
+      return remaining_time.round
+    elsif count > 2
+      time = (count - 2) * duration_seconds
+      total_remaining_time = time + remaining_time
+      return total_remaining_time.round
     else
       return 0
     end
