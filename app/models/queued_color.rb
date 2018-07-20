3fdfb28dda1abe5763ed27fb644e0e9864c8d08f
Kate Pond <kate@thepondsedge.com>
2018-07-20 09:53:34 -0700

    adds rainbow color

--- app/models/queued_color.rb
+++ app/models/queued_color.rb
@@ -77,6 +77,8 @@ class QueuedColor < ApplicationRecord
       return 11
     when '{"r":0,"g":102,"b":0,"a":1}' # '#006600'
       return 12
+    when 'rainbow' # rainbow colors
+      return 13
     else
       return 0
     end
